e317acdaf0979f371909b7753b49cc933475b656
Bruno Santos <brcesar19@gmail.com>
2020-11-05 19:16:23 -0300

    Create users

--- src/modules/customers/services/CreateCustomerService.ts
+++ src/modules/customers/services/CreateCustomerService.ts
@@ -15,7 +15,13 @@ class CreateCustomerService {
   constructor(private customersRepository: ICustomersRepository) {}
 
   public async execute({ name, email }: IRequest): Promise<Customer> {
-    // TODO
+    const customerExists = await this.customersRepository.findByEmail(email);
+
+    if (customerExists) throw new AppError('The email already exists');
+
+    const customer = await this.customersRepository.create({ name, email });
+
+    return customer;
   }
 }
 
